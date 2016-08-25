4f4fe90f4b373261758f20836801251d13247b10
Iyad <iejielat@gmail.com>
2016-08-25 18:20:41 +1000

    -test

--- src/Pluralsight/Startup.cs
+++ src/Pluralsight/Startup.cs
@@ -15,7 +15,7 @@ namespace Pluralsight
 
         public Startup()
         {
-            //
+            ///
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
